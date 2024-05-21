c15c5a531f514d47df9e1cf5babee934581b1350
Hasdae <estevancalderaro@gmail.com>
2024-05-21 11:56:54 -0300

    Testando Type Casting e Constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        long numeroLongo = numeroCurto;
+        short numeroTeste = (short) numeroLongo;
+        System.out.println(numeroTeste);
+        final double VALOR_DE_PI = 3.14;
+    
     }
 }
