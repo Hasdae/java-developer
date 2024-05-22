8935824ea07c9fa2992f30e92291acdb73efce65
Hasdae <estevancalderaro@gmail.com>
2024-05-22 17:46:37 -0300

    Atribuindo métodos para a classe

--- exemplo-smarttv/src/SmartTv.java
+++ exemplo-smarttv/src/SmartTv.java
@@ -1,5 +1,28 @@
 public class SmartTv {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        boolean ligada = false;
+        int canal = 1;
+        int volume = 25;
+
+        public void ligar(){
+            ligada = true;
+        }
+        public void desligar(){
+            ligada = false;
+        }
+        public void aumentarVolume(){
+            volume += 1; // pode ser usado volume ++
+        }
+        public void diminuirVolume(){
+            volume--; // pode ser usado volume -= 1
+        }
+        public void mudarCanal (int novoCanal){
+            canal = novoCanal;
+            System.out.println("Atribuindo novo canal");
+        }
+        public void aumentarCanal(){
+            canal++;
+        }
+        public void diminuirCanal(){
+            canal--;
+        }
 }
