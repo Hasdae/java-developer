df5af5094ae33d8f2dac0508007ca89c62ad9a64
Hasdae <estevancalderaro@gmail.com>
2024-05-28 12:47:02 -0300

    Desafio do curso Java Developer - DIO

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		}catch (ParametrosInvalidosException e) {
+			System.out.println("Parametro 1 deve ser menor que parametro 2");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+		int contagem = parametroDois - parametroUm;
+		for (int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo "+i+" de " +contagem);
+        }
+
+	}
+}
