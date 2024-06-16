803d496d93a2c369aabad2e4b574a29189193720
Hasdae <estevancalderaro@gmail.com>
2024-06-16 15:38:26 -0300

    Exemplo Springboot

---
+++ Modulo 7/Aulas/exemplo/src/test/java/edu/exemplo/ExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
