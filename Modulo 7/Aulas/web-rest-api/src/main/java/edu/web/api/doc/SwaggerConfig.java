a8293fa101680f1238651472e8f2c548aaaa93fa
Hasdae <estevancalderaro@gmail.com>
2024-06-28 15:58:38 -0300

    API Documentada com SWAGGER

---
+++ Modulo 7/Aulas/web-rest-api/src/main/java/edu/web/api/doc/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package edu.web.api.doc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info().title("Documentando API com Swagger").version("1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi detalheApi(){
+        return GroupedOpenApi.builder()
+                .group("http")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
